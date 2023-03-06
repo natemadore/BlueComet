3e429ed20e353535320ab87c81a4e6260dd580d8
Nathaniel Madore <natemadore@gmail.com>
2023-03-06 18:44:36 -0500

    Create BlueComet-Start-Linux.sh
    This requires sudo to run and attaches BlueComet as startup program on Linux.

---
+++ BlueComet-Start-Linux.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+
+#==================================================
+#=  ====  =====  =====       =====    ======    ===
+#=  ===  =====    ====  ====  ===  ==  ====  ==  ==
+#=  ==  =====  ==  ===  ====  ==  ====  ==  ====  =
+#=  =  =====  ====  ==  ====  ==  ====  ==  ====  =
+#=     =====  ====  ==       ===  ====  ==  ====  =
+#=  ==  ====        ==  ========  ====  ==  ====  =
+#=  ===  ===  ====  ==  ========  ====  ==  ====  =
+#=  ====  ==  ====  ==  =========  ==  ====  ==  ==
+#=  ====  ==  ====  ==  ==========    ======    ===
+#==================================================
+
+
+
+#!/bin/bash
+
+#==================================================
+#=  ====  =====  =====       =====    ======    ===
+#=  ===  =====    ====  ====  ===  ==  ====  ==  ==
+#=  ==  =====  ==  ===  ====  ==  ====  ==  ====  =
+#=  =  =====  ====  ==  ====  ==  ====  ==  ====  =
+#=     =====  ====  ==       ===  ====  ==  ====  =
+#=  ==  ====        ==  ========  ====  ==  ====  =
+#=  ===  ===  ====  ==  ========  ====  ==  ====  =
+#=  ====  ==  ====  ==  =========  ==  ====  ==  ==
+#=  ====  ==  ====  ==  ==========    ======    ===
+#==================================================
+
+# Compile the C++ code
+g++ -std=c++11 -o BlueComet BlueComet-Linux.cpp
+
+# Copy the executable to /usr/local/bin
+sudo cp BlueComet /usr/local/bin
+
+# Create the systemd service file
+sudo tee /etc/systemd/system/BlueComet.service <<EOF
+[Unit]
+Description=BlueComet
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/BlueComet
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload the systemd daemon
+sudo systemctl daemon-reload
+
+# Enable the service to run at startup
+sudo systemctl enable BlueComet.service
